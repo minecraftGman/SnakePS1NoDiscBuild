ae3777519d136711986267bdf3dbf4d7bb8a82a4
minecraftGman <83204508+minecraftGman@users.noreply.github.com>
2026-07-08 00:03:43 -0500

    Update AssetsAudio.c

--- psn00b_port/src/AssetsAudio.c
+++ psn00b_port/src/AssetsAudio.c
@@ -5,6 +5,3 @@ unsigned int bite1_vag_len = sizeof(bite1_vag);
 
 unsigned char die1_vag[] = { 0x00, 0x01, /* ... PASTE YOUR DATA HERE ... */ };
 unsigned int die1_vag_len = sizeof(die1_vag);
-
-unsigned char jungle_sounds_vag[] = { 0x00, 0x01, /* ... PASTE YOUR DATA HERE ... */ };
-unsigned int jungle_sounds_vag_len = sizeof(jungle_sounds_vag);
