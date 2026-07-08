64c6af3cf10477c35fa37055544b40a2d932e3ec
minecraftGman <83204508+minecraftGman@users.noreply.github.com>
2026-07-07 22:44:00 -0500

    Add files via upload

---
+++ psn00b_port/src/tmd.c
@@ -0,0 +1,9 @@
+#include "tmd.h"
+
+void load_tmd(uint8_t *data) {
+    // Stub for future TMD binary parsing
+}
+
+void draw_tmd(RenderContext *ctx, SVECTOR *rot, VECTOR *pos) {
+    // Stub for future TMD primitive rendering
+}
