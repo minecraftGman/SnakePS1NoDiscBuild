93d9a9c4f1b3fd4247a43640af76bef45c334f56
minecraftGman <83204508+minecraftGman@users.noreply.github.com>
2026-07-07 23:49:25 -0500

    Update AssetsAudio.c

--- psn00b_port/src/AssetsAudio.c
+++ psn00b_port/src/AssetsAudio.c
@@ -1,11 +1,10 @@
 #include <stddef.h>
 
-// These names must match your main.c exactly
-unsigned char bite1_vag[] = { /* PASTE YOUR HEX DATA FOR BITE1 HERE */ };
+unsigned char bite1_vag[] = { 0x00, 0x01, /* ... PASTE YOUR DATA HERE ... */ };
 unsigned int bite1_vag_len = sizeof(bite1_vag);
 
-unsigned char die1_vag[] = { /* PASTE YOUR HEX DATA FOR DIE1 HERE */ };
+unsigned char die1_vag[] = { 0x00, 0x01, /* ... PASTE YOUR DATA HERE ... */ };
 unsigned int die1_vag_len = sizeof(die1_vag);
 
-unsigned char jungle_vag[] = { /* PASTE YOUR HEX DATA FOR JUNGLE HERE */ };
-unsigned int jungle_vag_len = sizeof(jungle_vag);
+unsigned char jungle_sounds_vag[] = { 0x00, 0x01, /* ... PASTE YOUR DATA HERE ... */ };
+unsigned int jungle_sounds_vag_len = sizeof(jungle_sounds_vag);
