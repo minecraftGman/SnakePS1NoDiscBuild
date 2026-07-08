3285347de6075c53d47cfd2d64441e1a9d55b967
minecraftGman <83204508+minecraftGman@users.noreply.github.com>
2026-07-08 00:17:27 -0500

    Update AssetsAudio.c

--- psn00b_port/src/AssetsAudio.c
+++ psn00b_port/src/AssetsAudio.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 
-unsigned char bite1_vag[] = { 0x00, 0x01, /* ... PASTE YOUR DATA HERE ... */ };
+unsigned char bite1_vag[] = { /* PASTE BITE1 HEX HERE */ };
 unsigned int bite1_vag_len = sizeof(bite1_vag);
 
-unsigned char die1_vag[] = { 0x00, 0x01, /* ... PASTE YOUR DATA HERE ... */ };
+unsigned char die1_vag[] = { /* PASTE DIE1 HEX HERE */ };
 unsigned int die1_vag_len = sizeof(die1_vag);
