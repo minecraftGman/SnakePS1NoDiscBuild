924da24e4642bc73a7ae5615b1614f967274bbbf
minecraftGman <83204508+minecraftGman@users.noreply.github.com>
2026-07-08 00:42:31 -0500

    Update main.c

--- psn00b_port/src/main.c
+++ psn00b_port/src/main.c
@@ -36,17 +36,11 @@ static void setup_context(RenderContext *ctx) {
     SetDefDispEnv(&(ctx->buffers[0].disp), 0, 0, SCREEN_XRES, SCREEN_YRES);
     SetDefDrawEnv(&(ctx->buffers[1].draw), 0, SCREEN_YRES, SCREEN_XRES, SCREEN_YRES);
     SetDefDispEnv(&(ctx->buffers[1].disp), 0, SCREEN_YRES, SCREEN_XRES, SCREEN_YRES);
-    setRGB0(&(ctx->buffers[0].draw), 0, 0, 0); 
-    setRGB0(&(ctx->buffers[1].draw), 0, 0, 0);
-    ctx->buffers[0].draw.isbg = 1; 
-    ctx->buffers[1].draw.isbg = 1;
-    ctx->active = 0; 
-    ctx->next_packet = ctx->buffers[0].packets;
-    ClearOTagR(ctx->buffers[0].ot, OT_LEN); 
-    SetDispMask(1);
-    InitGeom(); 
-    gte_SetGeomOffset(SCREEN_XRES/2, SCREEN_YRES/2); 
-    gte_SetGeomScreen(256);
+    setRGB0(&(ctx->buffers[0].draw), 0, 0, 0); setRGB0(&(ctx->buffers[1].draw), 0, 0, 0);
+    ctx->buffers[0].draw.isbg = 1; ctx->buffers[1].draw.isbg = 1;
+    ctx->active = 0; ctx->next_packet = ctx->buffers[0].packets;
+    ClearOTagR(ctx->buffers[0].ot, OT_LEN); SetDispMask(1);
+    InitGeom(); gte_SetGeomOffset(SCREEN_XRES/2, SCREEN_YRES/2); gte_SetGeomScreen(256);
 }
 
 static void flip_buffers(RenderContext *ctx) {
@@ -54,8 +48,7 @@ static void flip_buffers(RenderContext *ctx) {
     RenderBuffer *draw = &(ctx->buffers[ctx->active]);
     PutDispEnv(&(ctx->buffers[ctx->active ^ 1].disp));
     DrawOTagEnv(&(draw->ot[OT_LEN - 1]), &(draw->draw));
-    ctx->active ^= 1; 
-    ctx->next_packet = ctx->buffers[ctx->active].packets;
+    ctx->active ^= 1; ctx->next_packet = ctx->buffers[ctx->active].packets;
     ClearOTagR(ctx->buffers[ctx->active].ot, OT_LEN);
 }
 
@@ -69,7 +62,6 @@ static void *alloc_packet(RenderContext *ctx, int z, size_t size) {
 
 static void loadTexture(unsigned char imageData[], TIM_IMAGE *out) {
     GetTimInfo((const uint32_t *) imageData, out);
-    // Loading at specific safe addresses
     LoadImage(out->prect, out->paddr); DrawSync(0);
     if (out->mode & 0x8) { LoadImage(out->crect, out->caddr); DrawSync(0); }
 }
@@ -115,15 +107,10 @@ int main(void) {
     RenderContext ctx; TIM_IMAGE tim;
     int s_x[MAX_SNAKE], s_y[MAX_SNAKE], s_len = 3, dx = 1, dy = 0, f_x = 5, f_y = 5, frames = 0, dead = 0, score = 0;
 
-    ResetGraph(0); 
-    setup_context(&ctx); 
-    initializePad(); 
-    SpuInit(); 
-    SpuSetTransferMode(SPU_TRANSFER_BY_DMA);
+    ResetGraph(0); setup_context(&ctx); initializePad(); SpuInit(); SpuSetTransferMode(SPU_TRANSFER_BY_DMA);
     
-    // Set VRAM for font to (960, 256) - standard PS1 BIOS font
     FntLoad(960, 256); 
-    font_id = FntOpen(0, 0, 320, 240, 0, 512); 
+    font_id = FntOpen(32, 32, 256, 200, 0, 512); 
     
     loadTexture(tex_loading, &tim);
     padReset();
@@ -150,9 +137,9 @@ int main(void) {
                 if (!dead && s_x[0] == f_x && s_y[0] == f_y) { s_len++; score += 10; f_x = (rand() % 18) - 9; f_y = (rand() % 12) - 6; play_sample(bite1_vag, bite1_vag_len, 1); }
                 for (int i = 1; i < s_len; i++) if (s_x[0] == s_x[i] && s_y[0] == s_y[i]) { play_sample(die1_vag, die1_vag_len, 2); dead = 1; }
             }
-            FntPrint("SCORE: %d", score);
+            FntPrint("SCORE: %d\n", score);
         } else {
-            FntPrint("GAME OVER\nSCORE: %d\nPRESS X TO RESTART", score);
+            FntPrint("GAME OVER\nSCORE: %d\nPRESS X TO RESTART\n", score);
             if (SysPadT & Pad1Cross) { dead = 0; s_len = 3; dx = 1; dy = 0; score = 0; for(int i=0;i<s_len;i++){s_x[i]=-i; s_y[i]=0;} }
         }
 
@@ -160,8 +147,7 @@ int main(void) {
         pos.vx = f_x * GRID_SIZE; pos.vy = f_y * GRID_SIZE; draw_cube(&ctx, &rot, &pos, 255, 0, 0);
         for (int i = 0; i < s_len; i++) { pos.vx = s_x[i] * GRID_SIZE; pos.vy = s_y[i] * GRID_SIZE; draw_cube(&ctx, &rot, &pos, 0, 255, 0); }
         
-        // Final flush using the active buffer's OT
-        FntFlush(ctx.buffers[ctx.active].ot);
+        FntFlush(font_id); 
         flip_buffers(&ctx);
     }
 }
