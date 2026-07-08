94e7ab038ca524e3ac32a15265bf727c5115bd9a
minecraftGman <83204508+minecraftGman@users.noreply.github.com>
2026-07-08 00:03:17 -0500

    Update tmd.h

--- psn00b_port/src/tmd.h
+++ psn00b_port/src/tmd.h
@@ -21,22 +21,8 @@ typedef struct {
     int          active;
 } RenderContext;
 
-typedef struct {
-    uint32_t id;
-    uint32_t flags;
-    uint32_t bck;
-    uint32_t num_objs;
-} TMD_HEADER;
-
-typedef struct {
-    uint32_t vert_top;
-    uint32_t num_verts;
-    uint32_t normal_top;
-    uint32_t num_normals;
-    uint32_t prim_top;
-    uint32_t num_prims;
-    int32_t  scale;
-} TMD_OBJ;
+typedef struct { uint32_t id; uint32_t flags; uint32_t bck; uint32_t num_objs; } TMD_HEADER;
+typedef struct { uint32_t vert_top; uint32_t num_verts; uint32_t normal_top; uint32_t num_normals; uint32_t prim_top; uint32_t num_prims; int32_t scale; } TMD_OBJ;
 
 void load_tmd(uint8_t *data);
 void draw_tmd(RenderContext *ctx, SVECTOR *rot, VECTOR *pos);
