bbd9307a5bae77432648516c0516cbe9a258f681
minecraftGman <83204508+minecraftGman@users.noreply.github.com>
2026-07-07 23:30:24 -0500

    Update tmd.c

--- psn00b_port/src/tmd.c
+++ psn00b_port/src/tmd.c
@@ -1,9 +1,7 @@
 #include "tmd.h"
 
 void load_tmd(uint8_t *data) {
-    // Stub for future TMD binary parsing
 }
 
 void draw_tmd(RenderContext *ctx, SVECTOR *rot, VECTOR *pos) {
-    // Stub for future TMD primitive rendering
 }
